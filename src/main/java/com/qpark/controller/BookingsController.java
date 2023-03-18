d9d387efb2780e15dd33a46d309f14f339748f4f
MManuel254 <127679954+MManuel254@users.noreply.github.com>
2023-03-18 14:11:41 +0300

    Changes to BookingController

--- src/main/java/com/qpark/controller/BookingsController.java
+++ src/main/java/com/qpark/controller/BookingsController.java
@@ -14,3 +14,36 @@ public class BookingsController extends HttpServlet {
         request.getRequestDispatcher("/views/booking.jsp").forward(request, response);
     }
 }
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+    
+    @Autowired
+    private BookingDAO bookingDAO;
+    
+    @PostMapping("")
+    public ResponseEntity<String> createBooking(@RequestBody Booking booking) {
+        bookingDAO.save(booking);
+        return new ResponseEntity<>("Booking created successfully", HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Booking> getBooking(@PathVariable("id") int id) {
+        Booking booking = bookingDAO.findById(id);
+        if (booking == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(booking, HttpStatus.OK);
+    }
+    
+    @GetMapping("")
+    public ResponseEntity<List<Booking>> getAllBookings() {
+        List<Booking> bookings = bookingDAO.findAll();
+        return new ResponseEntity<>(bookings, HttpStatus.OK);
+    }
+    
+    @PutMapping("/{id}/status")
+    public ResponseEntity<String> updateBookingStatus(@PathVariable("id") int id, @RequestParam("status") String status) {
+        Booking booking = bookingDAO.findById(id);
+        if (booking == null) {
+            return new ResponseEntity<>("Booking not found
